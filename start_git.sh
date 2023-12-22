c75da184eef3666782e2931c2058fe75f8104543
fanrencli <fanrencli@163.com>
2023-12-22 11:13:08 +0800

    20231222

--- start_git.sh
+++ start_git.sh
@@ -6,12 +6,13 @@ echo "begin at `date`" >$log_path
 echo "---- remote status ---------" >> $log_path
 git remote show origin >> $log_path
 flag=1
-if sed -n "/local out of date/p" $log_path
-then
-flag=0
-fi
+for line in `sed -n "/local out of date/p" $log_path`
+do  
+flag=0                             
+done
 if (($flag == 0))
 then
+echo "有文件更新！~~"
 git pull
 hexo g
 pm2 restart all
