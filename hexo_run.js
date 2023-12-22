b8dc4c4b041c99daf69b993dd5e8f3dc9f8e8aff
fanrencli <fanrencli@163.com>
2023-12-22 10:38:35 +0800

    20231222

--- hexo_run.js
+++ hexo_run.js
@@ -1,8 +1,9 @@
+const { exec } = require('child_process')
 exec('hexo server',(error, stdout, stderr) => {
-    if(error){
-            console.log('exec error: ${error}')
-            return
-    }
-    console.log('stdout: ${stdout}');
-    console.log('stderr: ${stderr}');
+        if(error){
+                console.log('exec error: ${error}')
+                return
+        }
+        console.log('stdout: ${stdout}');
+        console.log('stderr: ${stderr}');
 })
